ad883816491a1e9c10ebfe1ecdb13d300d4707d4
salehmu <ghd@keemail.me>
2022-09-11 04:40:01 +0200

    decouple custom file

---
+++ .doom.d/mycustom.el
@@ -0,0 +1,57 @@
+;;; ../configs/.doom.d/mycustom.el -*- lexical-binding: t; -*-
+
+
+(custom-set-variables ;; custom-set-variables was added by Custom.  ;; If you edit it by
+hand, you could mess it up, so be careful.  ;; Your init file should contain only one such
+instance.  ;; If there is more than one, they won't work right.
+'(bongo-mode-line-indicator-mode t) '(custom-safe-themes
+'("da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546"
+"f00a605fb19cb258ad7e0d99c007f226f24d767d01bf31f3828ce6688cbdeb22"
+"5a00018936fa1df1cd9d54bee02c8a64eafac941453ab48394e2ec2c498b834a"
+"2ce76d65a813fae8cfee5c207f46f2a256bac69dacbb096051a7a8651aa252b0"
+"3c7a784b90f7abebb213869a21e84da462c26a1fda7e5bd0ffebf6ba12dbd041"
+"cbdf8c2e1b2b5c15b34ddb5063f1b21514c7169ff20e081d39cf57ffee89bc1e"
+"6c98bc9f39e8f8fd6da5b9c74a624cbb3782b4be8abae8fd84cbc43053d7c175"
+"b186688fbec5e00ee8683b9f2588523abdf2db40562839b2c5458fcfb322c8a4"
+"e8653b24a63738f10c09496a3523ee1979ae335c6facc2c37a21e72fec9a49bb"
+"333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5"
+"c7737b9fc3471779c8e51ea0a37834d24aa80a0d6a79b215e7501227ada39855"
+"3770d0ae70172461ee0a02edcff71b7d480dc54066e8960d8de9367d12171efb"
+"bc26d791f0e8275d33226376a5010abbf13408494ed621f9efe256d26fcc873a"
+"246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c"
+"cc0dbb53a10215b696d391a90de635ba1699072745bf653b53774706999208e3"
+"a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8"
+"97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6"
+"613aedadd3b9e2554f39afe760708fc3285bf594f6447822dd29f947f0775d6c"
+"8146edab0de2007a99a2361041015331af706e7907de9d6a330a3493a541e5a6"
+"aba75724c5d4d0ec0de949694bce5ce6416c132bb031d4e7ac1c4f2dbdd3d580"
+"3e335d794ed3030fefd0dbd7ff2d3555e29481fe4bbb0106ea11c660d6001767"
+"4780d7ce6e5491e2c1190082f7fe0f812707fc77455616ab6f8b38e796cbffa9"
+"f91395598d4cb3e2ae6a2db8527ceb83fed79dbaf007f435de3e91e5bda485fb"
+"47e6f8c23eaea064b89ed1361b5824ee4f9562a8c4a30774ee9ee69f9b9d4f69"
+"569f33960d9d639e780ace5dcc2bfa834d4d7fad2084c147a9b1d86138e3014e"
+"1d89fcf0105dd8778e007239c481643cc5a695f2a029c9f30bd62c9d5df6418d"
+"67f0f440afa2e68d9d00219b5a56308761af45832fb60769d2b2fd36e3fead45"
+"8d7b028e7b7843ae00498f68fad28f3c6258eda0650fe7e17bfb017d51d0e2a2" default))
+'(highlight-indent-guides-method 'character) '(org-agenda-files
+'("/mnt/disk/leet/journal/20220408.org" "/mnt/disk/leet/journal/20220409.org"
+"/mnt/disk/leet/journal/20220410.org" "/mnt/disk/leet/journal/20220411.org"
+"/mnt/disk/leet/journal/20220413.org" "/mnt/disk/leet/journal/20220414.org"
+"/mnt/disk/leet/journal/20220416.org" "/mnt/disk/leet/journal/20220417.org"
+"/mnt/disk/leet/journal/20220508.org" "/mnt/disk/leet/journal/20220509.org"
+"/mnt/disk/leet/journal/20220510.org" "/mnt/disk/leet/journal/20220513.org"
+"/mnt/disk/leet/journal/20220524.org" "/mnt/disk/leet/journal/20220613.org"
+"/mnt/disk/leet/journal/20220709.org" "/mnt/disk/leet/journal/20220710.org"
+"/mnt/disk/leet/journal/20220718.org" "/mnt/disk/leet/journal/20220825.org"
+"/mnt/disk/leet/journal/20220827.org" "/mnt/disk/leet/journal/20220828.org"
+"/mnt/disk/leet/journal/20220829.org" "/mnt/disk/leet/journal/20220830.org"
+"/mnt/disk/leet/journal/20220831.org" "/mnt/disk/leet/journal/20220901.org"
+"/mnt/disk/leet/journal/20220903.org" "/mnt/disk/leet/journal/20220910.org"
+"/mnt/disk/leet/README.org" "/mnt/disk/leet/todo.org" "/mnt/Films/journal/20220911.org")))
+(put 'customize-set-variable 'disabled nil) (put 'customize-themes 'disabled nil) (put
+'customize-group 'disabled nil) (put 'customize-variable 'disabled nil) (custom-set-faces ;;
+custom-set-faces was added by Custom.  ;; If you edit it by hand, you could mess it up, so
+be careful.  ;; Your init file should contain only one such instance.  ;; If there is more
+than one, they won't work right.  ) (put 'customize-face 'disabled nil)
+
+(provide 'mycustom)
