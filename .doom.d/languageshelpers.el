e45d362f155ade91257c16cf6f2961f46080ec88
salehmu <ghd@keemail.me>
2022-09-18 04:48:22 +0200

    fix shell command switch

--- .doom.d/languageshelpers.el
+++ .doom.d/languageshelpers.el
@@ -207,8 +207,8 @@
     (setq eshell-path-env path-from-shell) ; for eshell users
     (setq exec-path (split-string path-from-shell path-separator))))
 
-(when window-system (set-exec-path-from-shell-PATH))
-(setq shell-command-switch "-ic")
+;(when window-system (set-exec-path-from-shell-PATH))
+;(setq shell-command-switch "-ic")
 
 
 
