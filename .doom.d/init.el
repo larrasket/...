afc7366025c9ca436738d5b640f1b497fe1fa3e6
salehmu <ghd@keemail.me>
2022-09-18 04:49:30 +0200

    load gist

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -97,7 +97,7 @@
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;; gist              ; interacting with github gists
+        gist              ; interacting with github gists
        (lookup
         +dictionary
         +offline)             ; navigate your code and its documentation
