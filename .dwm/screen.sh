329f8deb6371d700a66ff082e4c17bd8352d5147
salehmu <ghd@keemail.me>
2022-09-20 08:43:33 +0200

    Auto activate external monitor

---
+++ .dwm/screen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr
+xrandr --output HDMI-1-0 --auto --mode 1920x1080  --right-of eDP-1 
+#xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-0 --mode 1920x1080 --pos 1920x0 --rotate normal
