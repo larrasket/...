1eb082c4a743b6cab14bc2d4d3fb103c484e8f38
salehmu <ghd@keemail.me>
2022-10-05 06:55:41 +0200

    Fix splash image

--- .doom.d/genset.el
+++ .doom.d/genset.el
@@ -1,6 +1,7 @@
 ;;; genset.el -*- lexical-binding: t; -*-
 (provide 'genset)
 
+(setq fancy-splash-image "~/.doom.d/pan.png")
 ;; (setq-default fill-column 92)
 ;; (setq fill-column 92)
 (setq fill-column 110)
@@ -160,26 +161,6 @@
     (setq display-line-number-mode -1)))
 
 
-(setq fancy-splash-image "/home/ghd/me/wDfR_KdH_400x400.jpg")
-(use-package dashboard
-  :init      ;; tweak dashboard config before loading it
-  (setq dashboard-set-heading-icons t)
-  (setq dashboard-set-file-icons t)
-  (setq dashboard-banner-logo-title "Emacs Is More Than A Text Editor!")
-  ;;(setq dashboard-startup-banner 'logo) ;; use standard emacs logo as banner
-  (setq dashboard-startup-banner "/home/ghd/me/wDfR_KdH_400x400.jpg")  ;; use custom image as banner
-  (setq dashboard-center-content nil) ;; set to 't' for centered content
-  (setq dashboard-items '((recents . 5)
-                          (agenda . 5 )
-                          (bookmarks . 3)
-                          (projects . 3)
-                          (registers . 3)))
-  :config
-  (dashboard-setup-startup-hook)
-  (dashboard-modify-heading-icons '((recents . "file-text")
-			      (bookmarks . "book"))))
-(setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
-(setq doom-fallback-buffer-name "*dashboard*")
 
 
 
