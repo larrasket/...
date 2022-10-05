1601d499ee3067ce11b08b9b306ab173c3189f29
salehmu <ghd@keemail.me>
2022-10-05 07:23:44 +0200

    Dump

--- .config/mpv/scripts/run.sh
+++ .config/mpv/scripts/run.sh
@@ -1,8 +1,8 @@
-cd "/home/ghd/me/cinema/Movies/Blue Valentine (2010)"
+cd "/home/ghd/me/cinema/Shows/Black Mirror/S03"
 mkdir .cut_video
-ffmpeg -ss 1167.249 -i /home/ghd/me/cinema/Movies/Blue\ Valentine\ \(2010\)/Blue.Valentine.BrRip.720p.x264.YIFY.mkv -t 35.244 -c  copy -avoid_negative_ts 1 .cut_video/clip0.mkv
+ffmpeg -ss 2855.083 -i /home/ghd/me/cinema/Shows/Black\ Mirror/S03/Black.Mirror.S03E03.720p.BluRay.x264-GalaxyTV.mkv -t 252.000 -c  copy -avoid_negative_ts 1 .cut_video/clip0.mkv
 echo "file 'clip0.mkv'" >>.cut_video/concat.txt
-cp .cut_video/clip0.mkv "`date "+%Y_%m_%d_%H_%M_%S"`_cut_Blue.Valentine.BrRip.720p.x264.YIFY.mkv"
+cp .cut_video/clip0.mkv "`date "+%Y_%m_%d_%H_%M_%S"`_cut_Black.Mirror.S03E03.720p.BluRay.x264-GalaxyTV.mkv"
 rm -rf .cut_video
 echo OK!
 echo srcipt_dir:/home/ghd/.config/mpv/scripts
