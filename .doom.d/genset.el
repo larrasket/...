bea799e3fa44c4410eae85a22ae71d9a8faaa18e
salehmu <ghd@keemail.me>
2022-10-08 00:55:44 +0200

    Disable music setup for Amarok

--- .doom.d/genset.el
+++ .doom.d/genset.el
@@ -2,6 +2,9 @@
 (provide 'genset)
 ;; (after! doom-modeline
   ;; (setq doom-modeline-icon nil))
+
+(setq-default frame-title-format '("%b"))
+(add-to-list 'doom-emoji-fallback-font-families "Symbola")
 (setq fancy-splash-image "~/.doom.d/pan.png")
 ;; (setq-default fill-column 92)
 ;; (setq fill-column 92)
@@ -177,184 +180,6 @@
 
 (setq auto-save-visited-mode t)
 
-
-
-
-
-
-;; ;; tabs
-
-;; (add-hook 'dired-mode-hook 'centaur-tabs-local-mode)
-;; (add-hook 'dired-sidebar-mode 'centaur-tabs-local-mode)
-;; (add-hook 'org-agenda-mode-hook 'centaur-tabs-local-mode)
-;; (centaur-tabs-mode t)
-;; (setq centaur-tabs-set-modified-marker t)
-
-;;     (defun centaur-tabs-buffer-groups ()
-;;       "`centaur-tabs-buffer-groups' control buffers' group rules.
-
-;;     Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
-;;     All buffer name start with * will group to \"Emacs\".
-;;     Other buffer group by `centaur-tabs-get-group-name' with project name."
-;;       (list
-;; 	(cond
-;; 	 ((or (string-equal "*" (substring (buffer-name) 0 1))
-;; 	      (memq major-mode '(magit-process-mode
-;; 				 magit-status-mode
-;; 				 magit-diff-mode
-;; 				 magit-log-mode
-;; 				 magit-file-mode
-;; 				 magit-blob-mode
-;; 				 magit-blame-mode
-;; 				 )))
-;; 	  "Emacs")
-;; 	 ((derived-mode-p 'prog-mode)
-;; 	  "Editing")
-;; 	 ((derived-mode-p 'dired-mode)
-;; 	  "Dired")
-;; 	 ((memq major-mode '(helpful-mode
-;; 			     help-mode))
-;; 	  "Help")
-;; 	 ((memq major-mode '(org-mode
-;; 			     org-agenda-clockreport-mode
-;; 			     org-src-mode
-;; 			     org-agenda-mode
-;; 			     org-beamer-mode
-;; 			     org-indent-mode
-;; 			     org-bullets-mode
-;; 			     org-cdlatex-mode
-;; 			     org-agenda-log-mode
-;; 			     diary-mode))
-;; 	  "OrgMode")
-;; 	 (t
-;; 	  (centaur-tabs-get-group-name (current-buffer))))))
-
-;; ;; (setq centaur-tabs-gray-out-icons 'buffer)
-
-
-;; (setq centaur-tabs-set-icons t)
-;; (setq centaur-tabs-plain-icons t)
-;; (setq centaur-tabs-set-close-button nil)
-
-
-
-
-;; (defun centaur-tabs-hide-tab (x)
-;;   "Do no to show buffer X in tabs."
-;;   (let ((name (format "%s" x)))
-;;     (or
-;;      ;; Current window is not dedicated window.
-;;      (window-dedicated-p (selected-window))
-
-;;      ;; Buffer name not match below blacklist.
-;;      ;; (string-prefix-p "*epc" name)
-;;      ;; (string-prefix-p "*helm" name)
-;;      ;; (string-prefix-p "*Helm" name)
-;;      ;; (string-prefix-p "*Compile-Log*" name)
-;;      ;; (string-prefix-p "*lsp" name)
-;;      ;; (string-prefix-p "*company" name)
-;;      ;; (string-prefix-p "*Flycheck" name)
-;;      ;; (string-prefix-p "*tramp" name)
-;;      ;; (string-prefix-p " *Mini" name)
-;;      ;; (string-prefix-p "*help" name)
-;;      ;; (string-prefix-p "*straight" name)
-;;      ;; (string-prefix-p " *temp" name)
-;;      ;; (string-prefix-p "*Help" name)
-;;      ;; (string-prefix-p "*mybuf" name)
-;;      ;; (string-prefix-p "*leetcode*" name)
-;;      (string-prefix-p "*doom*" name)
-;;      (string-prefix-p "*scratch*" name)
-;;      (string-prefix-p "*Messages*" name)
-;;      (string-prefix-p "*LSP Symbols List*" name)
-;;      (string-prefix-p "*omnisharp*" name)
-
-;;      ;; Is not magit buffer.
-;;      (and (string-prefix-p "magit" name)
-;; 	  (not (file-name-extension name)))
-;;      )))
-
-
-
-
-
-;;  (use-package centaur-tabs
-;;    :load-path "~/.emacs.d/other/centaur-tabs"
-;;    :config
-;;    (setq centaur-tabs-style "bar"
-;; 	  centaur-tabs-height 19
-;; 	  centaur-tabs-set-icons nil
-;; 	  centaur-tabs-set-modified-marker t
-;; 	  ;; centaur-tabs-show-navigation-buttons t
-;; 	  centaur-tabs-set-bar 'under
-;; 	  x-underline-at-descent-line t)
-;;    ;; (centaur-tabs-headline-match)
-;;    ;; (setq centaur-tabs-gray-out-icons 'buffer)
-;;    ;; (centaur-tabs-enable-buffer-reordering)
-;;    ;; (setq centaur-tabs-adjust-buffer-order t)
-;;    (centaur-tabs-mode t)
-;;    (setq uniquify-separator "/")
-;;    (setq uniquify-buffer-name-style 'forward)
-;;    (defun centaur-tabs-buffer-groups ()
-;;      (list
-;;       (cond
-;; 	((or (string-equal "*" (substring (buffer-name) 0 1))
-;; 	     (memq major-mode '(magit-process-mode
-;; 				magit-status-mode
-;; 				magit-diff-mode
-;; 				magit-log-mode
-;; 				magit-file-mode
-;; 				magit-blob-mode
-;; 				magit-blame-mode
-;; 				)))
-;; 	 "Emacs")
-;; 	((derived-mode-p 'prog-mode)
-;; 	 "Editing")
-;; 	((derived-mode-p 'dired-mode)
-;; 	 "Dired")
-;; 	((memq major-mode '(helpful-mode
-;; 			    help-mode))
-;; 	 "Help")
-;; 	((memq major-mode '(org-mode
-;; 			    org-agenda-clockreport-mode
-;; 			    org-src-mode
-;; 			    org-agenda-mode
-;; 			    org-beamer-mode
-;; 			    org-indent-mode
-;; 			    org-bullets-mode
-;; 			    org-cdlatex-mode
-;; 			    org-agenda-log-mode
-;; 			    diary-mode))
-;; 	 "OrgMode")
-;; 	(t
-;; 	 (centaur-tabs-get-group-name (current-buffer))))))
-;;    :hook
-;;    (dashboard-mode . centaur-tabs-local-mode)
-;;    (term-mode . centaur-tabs-local-mode)
-;;    (calendar-mode . centaur-tabs-local-mode)
-;;    (org-agenda-mode . centaur-tabs-local-mode)
-;;    (helpful-mode . centaur-tabs-local-mode)
-;;    :bind
-;;    ("C-<prior>" . centaur-tabs-backward)
-;;    ("C-<next>" . centaur-tabs-forward)
-;;    ("C-c t s" . centaur-tabs-counsel-switch-group)
-;;    ("C-c t p" . centaur-tabs-group-by-projectile-project)
-;;    ("C-c t g" . centaur-tabs-group-buffer-groups)
-;;    (:map evil-normal-state-map
-;; 	  ("g t" . centaur-tabs-forward)
-;; 	  ("g T" . centaur-tabs-backward)))
-
-
-
-
-;; (add-hook! 'doom-init-ui-hook
-;;   (centaur-tabs-change-fonts "Inconsolata" 76)
-;;   )
-
-;; git integeration
-
-
-;; (add-hook 'prog-mode-hook 'git-gutter-mode)
-
 (use-package git-gutter
   :hook (prog-mode . git-gutter-mode)
   :config
@@ -365,4 +190,3 @@
   (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
-;; (setq-default org-download-image-dir 'nil)
