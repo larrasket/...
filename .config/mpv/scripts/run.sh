d7c939e97e4a73d341a056d0ae7c2fc21be2b5be
salehmu <ghd@keemail.me>
2022-10-14 12:20:04 +0200

    Fix mpv keybindings

--- .config/mpv/scripts/run.sh
+++ .config/mpv/scripts/run.sh
@@ -1,8 +1,6 @@
-cd "/home/ghd/me/cinema/Shows/Black Mirror/S03"
+cd "/home/ghd/me/cinema/Shows/Better Call Saul/S05"
 mkdir .cut_video
-ffmpeg -ss 2855.083 -i /home/ghd/me/cinema/Shows/Black\ Mirror/S03/Black.Mirror.S03E03.720p.BluRay.x264-GalaxyTV.mkv -t 252.000 -c  copy -avoid_negative_ts 1 .cut_video/clip0.mkv
-echo "file 'clip0.mkv'" >>.cut_video/concat.txt
-cp .cut_video/clip0.mkv "`date "+%Y_%m_%d_%H_%M_%S"`_cut_Black.Mirror.S03E03.720p.BluRay.x264-GalaxyTV.mkv"
+ffmpeg -f concat -i .cut_video/concat.txt -c copy "`date "+%Y_%m_%d_%H_%M_%S"`_cut_Better.Call.Saul.S05E06.720p.NF.WEBRip.x264-GalaxyTV.mkv"
 rm -rf .cut_video
 echo OK!
 echo srcipt_dir:/home/ghd/.config/mpv/scripts
