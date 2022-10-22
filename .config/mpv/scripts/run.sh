10be897c26d327d4c7c608f7fa7d6cf6cfce61ec
salehmu <ghd@keemail.me>
2022-10-23 01:28:07 +0200

    Remove junk

--- .config/mpv/scripts/run.sh
+++
@@ -1,8 +0,0 @@
-cd "/home/ghd/me/cinema/Movies/archive/Being There (1979)"
-mkdir .cut_video
-ffmpeg -ss 5340.12 -i /home/ghd/me/cinema/Movies/archive/Being\ There\ \(1979\)/Being.There.1979.512x288.25fps.689kbs.88mp3.MultiSub.WunSeeDee.avi -t 46.28 -c  copy -avoid_negative_ts 1 .cut_video/clip0.avi
-echo "file 'clip0.avi'" >>.cut_video/concat.txt
-cp .cut_video/clip0.avi "`date "+%Y_%m_%d_%H_%M_%S"`_cut_Being.There.1979.512x288.25fps.689kbs.88mp3.MultiSub.WunSeeDee.avi"
-rm -rf .cut_video
-echo OK!
-echo srcipt_dir:/home/ghd/.config/mpv/scripts
