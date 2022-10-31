25aa5196dda3a4c5ae0970c2630b487812c29cf7
salehmu <ghd@keemail.me>
2022-11-01 01:27:02 +0200

    Install Docker model

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -82,7 +82,7 @@
        ;;ansible
        ;;(debugger +lsp)          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;; docker
+       docker
        editorconfig                  ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        ;; (eval +overlay)     ; run code, run (also, repls)
