113fb63c463a4ac32f34a619476bc7f4d947b835
salehmu <ghd@keemail.me>
2022-11-05 01:00:33 +0200

    Remove upcoming

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,11 +3,6 @@
 (require 'themeconf)
 (require 'leader)
 (require 'keys)
-;; (use-package org-upcoming-modeline
-;;   :after org
-;;   :load-path "~/gits/org-upcoming-modeline"
-;;   :config
-;;   (org-upcoming-modeline-mode))
 (setq org-agenda-files (directory-files-recursively "~/roam/journal" "\\.org$"))
 (setq +org-capture-journal-file "~/blog/content/stack.org")
 (setq +org-capture-changelog-file "~/blog/content/nice.org")
