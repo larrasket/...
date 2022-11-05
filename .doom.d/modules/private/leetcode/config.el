1831ee61f2a9fe68a1c57dc2e8b707b58f181611
salehmu <ghd@keemail.me>
2022-11-05 05:36:25 +0200

    Fix leetcode location
    Modified-by: salehmu <ghd@keemail.me>

--- .doom.d/modules/private/leetcode/config.el
+++ .doom.d/modules/private/leetcode/config.el
@@ -4,7 +4,7 @@
 (setq leetcode-prefer-language "cpp")
 (setq leetcode-prefer-sql "mssql")
 (setq leetcode-save-solutions t)
-(setq leetcode-directory "/home/ghd/temp/leet/ps/lc")
+(setq leetcode-directory "/home/ghd/gits/leet/ps/lc")
 
 (map! :leader
       :desc "open leetcode"
