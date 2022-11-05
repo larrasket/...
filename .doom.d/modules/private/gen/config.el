7ed3703fcc597352398c7facbe6a9b9c841d0d84
salehmu <ghd@keemail.me>
2022-11-05 06:41:24 +0200

    Fix highlight-indent-guides-colors

--- .doom.d/modules/private/gen/config.el
+++ .doom.d/modules/private/gen/config.el
@@ -91,11 +91,12 @@
 (require 'highlight-indent-guides)
 (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
 
-(set-face-background 'highlight-indent-guides-odd-face "#9B9C97")
-(set-face-background 'highlight-indent-guides-even-face "#9B9C97")
-(set-face-foreground 'highlight-indent-guides-character-face "#9B9C97")
-
-
+;; (set-face-background 'highlight-indent-guides-odd-face "#9B9C97")
+;; (set-face-background 'highlight-indent-guides-even-face "#9B9C97")
+;; (set-face-foreground 'highlight-indent-guides-character-face "#9B9C97")
+(set-face-background 'highlight-indent-guides-odd-face "darkgray")
+(set-face-background 'highlight-indent-guides-even-face "dimgray")
+(set-face-foreground 'highlight-indent-guides-character-face "dimgray")
 
 
 
