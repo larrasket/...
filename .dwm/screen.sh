d17ea9eec151954ef291da8b16aad8ee83032135
salehmu <ghd@keemail.me>
2023-01-23 23:21:37 +0200

    Setup screeen

--- .dwm/screen.sh
+++ .dwm/screen.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-xrandr
-#xrandr --output HDMI-1-0 --auto --mode 1920x1080  --right-of eDP-1 
-feh --bg-fill ~/configs/bg.png
-#xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-0 --mode 1920x1080 --pos 1920x0 --rotate normal
-#
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1-0 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-0 --mode 1920x1080 --pos 1920x0 --rotate normal
