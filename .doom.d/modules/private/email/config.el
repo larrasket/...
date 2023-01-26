561f624fe5a6d955ce0fda886bc75f9bf1679b5e
salehmu <ghd@keemail.me>
2023-01-26 06:53:03 +0200

    Fix email client

--- .doom.d/modules/private/email/config.el
+++ .doom.d/modules/private/email/config.el
@@ -66,16 +66,28 @@
 
 (setq mail-user-agent 'gnus-user-agent)
 (require 'org-msg)
+
+
+
+
+
+
+(setq mail-user-agent 'gnus-user-agent)
+(require 'org-msg)
 (setq org-msg-options "html-postamble:nil H:5 num:nil ^:{} toc:nil author:nil email:nil \\n:t"
-  	org-msg-startup "hidestars indent inlineimages"
-  	org-msg-greeting-name-limit 3
-  	org-msg-default-alternatives '((new    		. (text html)))
-               (reply-to-html  	. (text html))
-               (reply-to-text  	. (text))
-  	org-msg-convert-citation t
-  	org-msg-signature "
+	org-msg-startup "hidestars indent inlineimages"
+	org-msg-greeting-fmt "\nHi%s,\n\n"
+	org-msg-greeting-name-limit 3
+	org-msg-default-alternatives '((new		. (text html))
+				       (reply-to-html	. (text html))
+				       (reply-to-text	. (text)))
+	org-msg-convert-citation t
+	org-msg-signature "
+
 Regards,
 
 #+begin_signature
-Salih Muhammed
+--
+*Salih Muhammed*
 #+end_signature")
+(org-msg-mode)
