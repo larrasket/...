6b83486e47a695249f78cf500d219e97de3dcc44
salehmu <ghd@keemail.me>
2023-02-02 17:29:42 +0200

    Delete junk

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -50,48 +50,6 @@
   (vertigo-mode 1)
   (setq vertigo-completing-read-function 'ivy-completing-read))
 
-
-;; Choose some fonts
-;; (set-face-attribute 'default nil :family "Iosevka")
-;; (set-face-attribute 'variable-pitch nil :family "Iosevka Aile")
-;; (set-face-attribute 'org-modern-symbol nil :family "Iosevka")
-
-;; Add frame borders and window dividers
-;; (modify-all-frames-parameters
-;;  '((right-divider-width . 40)
-;;    (internal-border-width . 40)))
-;; (dolist (face '(window-divider
-;;                 window-divider-first-pixel
-;;                 window-divider-last-pixel))
-;;   (face-spec-reset-face face)
-;;   (set-face-foreground face (face-attribute 'default :background)))
-;; (set-face-background 'fringe (face-attribute 'default :background))
-
-;; (setq
-;;  ;; Edit settings
-;;  org-auto-align-tags nil
-;;  org-tags-column 0
-;;  org-catch-invisible-edits 'show-and-error
-;;  org-special-ctrl-a/e t
-;;  org-insert-heading-respect-content t
-
-;;  ;; Org styling, hide markup etc.
-;;  org-hide-emphasis-markers t
-;;  org-pretty-entities t
-;;  org-ellipsis "…"
-
-;;  ;; Agenda styling
-;;  org-agenda-tags-column 0
-;;  org-agenda-block-separator ?─
-;;  org-agenda-time-grid
-;;  '((daily today require-timed)
-;;    (800 1000 1200 1400 1600 1800 2000)
-;;    " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
-;;  org-agenda-current-time-string
-;;  "⭠ now ─────────────────────────────────────────────────")
-
-;; Choose some fonts
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
