56fb5f41c32d90401b14f505ee7d410ef33563d1
salehmu <ghd@keemail.me>
2023-02-02 17:30:32 +0200

    Separate mssql from sql

---
+++ .doom.d/modules/private/mssql/config.el
@@ -0,0 +1,10 @@
+;;; private/mssql/config.el -*- lexical-binding: t; -*-
+
+
+
+(require 'lsp-mssql)
+(setq lsp-mssql-connections
+      [(:server "172.21.0.3"
+        :database "master"
+        :user "sa"
+        :password "rBwiY3JgqmG26q@")])
