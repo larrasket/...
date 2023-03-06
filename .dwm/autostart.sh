cf04ce556d2b8ec57d5119d1a7d64853247402d6
salehmu <ghd@keemail.me>
2023-03-06 20:02:27 +0200

    Use systemctl to start tor

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -11,6 +11,6 @@ setxkbmap -layout en_US,ar -option 'grp:alt_shift_toggle'
 /home/ghd/go/bin/notifier  &
 #/home/ghd/go/bin/motivier  &
 feh --bg-fill ~/configs/bg.png
-tor &
+# tor &
 # pgrep -fl 'pidswallow -gl' || pidswallow -gl
 flameshot &
