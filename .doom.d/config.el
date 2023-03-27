6b95b5327b4db13b2f984ff841d84a9a1f567849
salehmu <ghd@keemail.me>
2023-03-27 22:56:16 +0200

    Set IRC

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -2,67 +2,74 @@
 (add-to-list 'org-agenda-files "~/roam/journal/agenda/todo.org")
 (add-to-list 'org-agenda-files "~/roam/journal/agenda/births.org")
 (require 'epa-file)
-
+(require 'erc-services)
 
 
 
 (setq load-prefer-newer t ;; avoid warnings
-      ;; set org files
-      +org-capture-journal-file                         "~/blog/content/stack.org"
-      +org-capture-changelog-file                       "~/blog/content/nice.org"
-      +org-capture-todo-file                            "~/roam/main/life.org"
-      org-preview-html-viewer                           'xwidget
-      org-roam-directory                                "~/roam"
-      org-extend-today-until                            2
+     ;; set org files
+     +org-capture-journal-file                         "~/blog/content/stack.org"
+     +org-capture-changelog-file                       "~/blog/content/nice.org"
+     +org-capture-todo-file                            "~/roam/main/life.org"
+     org-preview-html-viewer                           'xwidget
+     org-roam-directory                                "~/roam"
+     org-extend-today-until                            2
 
 
-      ;; please don't stalk me
-      user-full-name                                    "Salih Muhammed"
-      user-mail-address                                 "salhghd7@gmail.com"
+     ;; please don't stalk me
+     user-full-name                                    "Salih Muhammed"
+     user-mail-address                                 "salhghd7@gmail.com"
 
-      ;; I've no idea of any of this.
-      org-crypt-key                                     user-mail-address
-      epa-file-cache-passphrase-for-symmetric-encryption t
-      epa-file-select-keys                              'silent
-      epa-file-encrypt-to                               user-mail-address
-
-
-      ;; skull welecome in emacs
-      fancy-splash-image                                "~/.doom.d/pan.png"
-
-      ;; theme
-      doom-theme                                        'distinguished
-      highlight-indent-guides-method                    'bitmap
-
-      ;; prayer time
-      calendar-latitude                                 30.0
-      calendar-longitude                                31.2
-
-      ;; school
-      bibtex-completion-bibliography                    "~/configs/ref.bib"
-
-      ;; translate
-      gts-translate-list                                '(("en" "ar"))
-
-      ;; keyboard
-      salih/prefix-global                               "C-x "
-      salih/prefix-mode                                 "C-c "
+     ;; I've no idea of any of this.
+     org-crypt-key                                     user-mail-address
+     epa-file-cache-passphrase-for-symmetric-encryption t
+     epa-file-select-keys                              'silent
+     epa-file-encrypt-to                               user-mail-address
 
 
-      ;; other
-      vertico-buffer-display-action                     '(display-buffer-same-window)
-      browse-url-generic-program                        "chromium"
-      large-file-warning-threshold                      nil
-      inferior-lisp-program                             "sbcl"
-      neo-mode-line-type                                'default
-      consult-preview-key                               nil
-      treemacs-position                                 'right
-      dired-sidebar-refresh-on-special-commands         t
-      display-line-numbers-type                         t
-      doom-modeline-height                              17
-      doom-modeline-buffer-state-icon                   nil
-      doom-modeline-icon                                nil
-      org-annotate-file-storage-file                    "~/configs/annotated.org")
+     ;; skull welecome in emacs
+     fancy-splash-image                                "~/.doom.d/pan.png"
+
+     ;; theme
+     doom-theme                                        'distinguished
+     highlight-indent-guides-method                    'character
+
+     ;; prayer time
+     calendar-latitude                                 30.0
+     calendar-longitude                                31.2
+
+     ;; school
+     bibtex-completion-bibliography                    "~/configs/ref.bib"
+
+     ;; translate
+     gts-translate-list                                '(("en" "ar"))
+
+     ;; keyboard
+     salih/prefix-global                               "C-x "
+     salih/prefix-mode                                 "C-c "
+
+
+     ;; irc
+     erc-nick                                          "jahiz"
+     erc-user-full-name                                "Salih Muhammed"
+     erc-prompt-for-password                            nil
+     erc-prompt-for-nickserv-password                   nil
+     erc-autojoin-channels-alist                        '(("irc.libera.chat" "##arabic"))
+
+     ;; other
+     vertico-buffer-display-action                     '(display-buffer-same-window)
+     browse-url-generic-program                        "chromium"
+     large-file-warning-threshold                      nil
+     inferior-lisp-program                             "sbcl"
+     neo-mode-line-type                                'default
+     consult-preview-key                               nil
+     treemacs-position                                 'right
+     dired-sidebar-refresh-on-special-commands         t
+     display-line-numbers-type                         t
+     doom-modeline-height                              17
+     doom-modeline-buffer-state-icon                   nil
+     doom-modeline-icon                                nil
+     org-annotate-file-storage-file                    "~/configs/annotated.org")
 
 
 ;; setup email
@@ -85,6 +92,9 @@
   (setq solaire-mode-real-buffer-fn #'salih/solaire-mode-real-buffer-custom-p))
 
 
+(after! sly
+  (setq sly-complete-symbol-function 'sly-flex-completions))
+
 ;; this should be called after defining salih/prefix-global
 (require '+handy)
 (require 'keys)
@@ -99,27 +109,16 @@
 (add-hook 'nov-mode-hook     #'salih/make-buffer-white)
 (add-hook 'csv-mode-hook     #'csv-align-mode)
 (add-hook 'prog-mode-hook    (lambda () (setq-default indent-tabs-mode nil)))
-(add-hook 'org-mode-hook     (lambda () (display-line-numbers-mode -1) (setq truncate-lines 1)))
+(add-hook 'org-mode-hook     (lambda ()
+                               (display-line-numbers-mode -1)
+                               (setq truncate-lines 1)))
 
 (epa-file-enable)
 (global-wakatime-mode)
 (vertico-buffer-mode)
 (salih/consult-preview-at-point)
+(erc-spelling-mode 1)
 
-;; (after! lsp-mode
-;;   (push "[/\\\\]_build\\'" lsp-file-watch-ignored-directories)
-;;   (push "[/\\\\]deps\\'" lsp-file-watch-ignored-directories)
-;;   (push "[/\\\\]cover\\'" lsp-file-watch-ignored-directories)
-;;   (push "[/\\\\]priv\\'" lsp-file-watch-ignored-directories)
-;;   (push "[/\\\\]\\.deliver\\'" lsp-file-watch-ignored-directories)
-;;   (push "[/\\\\]\\.elixir_ls\\'" lsp-file-watch-ignored-directories)
-;;   (push "[/\\\\]\\.build\\'" lsp-file-watch-ignored-directories)
-;;   (push "[/\\\\]node_modules\\'" lsp-file-watch-ignored-directories)
-;;   (push "[/\\\\]examples\\'" lsp-file-watch-ignored-directories))
+
+
 (add-hook 'after-init-hook #'mu4e)
-
-(after! sly
-  (setq sly-complete-symbol-function 'sly-flex-completions))
-
-
-
