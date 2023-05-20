3e5f5f6a2ec183a4c87af86d592e5f1898ce22b6
salehmu <ghd@keemail.me>
2023-05-20 05:40:17 +0300

    Add org-roam binding

--- ~s/.doom.d/+bindings.el
+++ ~s/.doom.d/+bindings.el
@@ -55,6 +55,7 @@
             (local-set-key (salih/mode "i l") #'org-web-tools-insert-link-for-url)
             (local-set-key (salih/mode "i d") #'org-download-clipboard)
             (local-set-key (salih/mode "i k") #'citar-insert-citation)
+            (local-set-key (salih/mode "i r") #'org-roam-node-insert)
             (local-set-key (salih/global "TAB") #'consult-org-heading)))
 
 (add-hook 'ement-room-mode-hook
