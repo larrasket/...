76a0f0157e771345d96ff56914e5839e361e0d55
salehmu <ghd@keemail.me>
2023-06-20 11:53:52 +0300

    chore(dots) update screen settings

--- ~s/.dwm/screen.sh
+++ ~s/.dwm/screen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xrandr --newmode "1920x1080_60.00" 172.80 1920 2040 2248 2576 1080 1081 1084 1118 -HSync +Vsync
 xrandr --addmode DP-3 "1920x1080_60.00"
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --primary --mode 1920x1080 --pos 3840x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-3 --mode 1920x1080_60.00 --pos 1920x0 --rotate normal --output HDMI-3 --off
+xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-3 --mode 1920x1080_60.00 --pos 3840x0 --rotate normal --output HDMI-3 --off
