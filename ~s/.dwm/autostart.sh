2df9219c637bf2a7d7aed9e7be4c9211831efe21
Salih Muhammed <lr0@gmx.com>
2023-08-05 11:47:15 +0300

    chore(dots): make background transparent

--- ~s/.dwm/autostart.sh
+++ ~s/.dwm/autostart.sh
@@ -10,7 +10,9 @@ xset r rate 200 25
 setxkbmap -layout us,ara -option 'grp:alt_shift_toggle'
 /home/l/.local/bin/pantalaimon &
 /home/l/go/bin/ang  &
-feh --bg-fill ~/configs/bg.png
+
+feh --bg-fill ~/me/links/art/land/$(ls ~/me/links/art/land | shuf -n 1)
+
 # tor &
 # pgrep -fl 'pidswallow -gl' || pidswallow -gl
 pantalaimon --data-path ~/tmp/pant/ &
