6b416dd5c50aaaabee0b22eb9200d4a9c47274b2
Salih Muhammed <lr0@gmx.com>
2023-08-12 13:52:19 +0300

    feat(emacs): new rss feed link generator

--- ~s/.doom.d/lisp/+feeds.el
+++ ~s/.doom.d/lisp/+feeds.el
@@ -11,23 +11,14 @@
                      ("https://williamdavies.blog/feed/" philosophy)
                      ("http://chomsky.info/feed/" philosophy)
                      ("https://blog.giovanh.com/feeds/atom.xml" cs)
+                     ("https://hnrss.org/replies?id=lr0" personal)
 
-                     ;; ("https://boards.4channel.org/sci/index.rss" science)
-                     ;; ("https://stallman.org/rss/rss.xml" politics)
-                     ("https://old.reddit.com/r/emacs/.rss" emacs)
                      ("https://www.youtube.com/feeds/videos.xml?channel_id=UCdHT7KB1gDAXZYpPW71fn0Q" yt philosophy)
                      ("https://www.youtube.com/feeds/videos.xml?channel_id=UC9-y-6csu5WGm29I7JiwpnA" yt cs)
-
                      ("https://www.youtube.com/feeds/videos.xml?channel_id=UCVpb6nv1igxnoY0SRVhaoEA" yt cs)
-
                      ("https://www.youtube.com/feeds/videos.xml?channel_id=UC_5ENzPnzYCGRxiOIdtaRug" yt aesthetic)
                      ("https://www.youtube.com/feeds/videos.xml?channel_id=UC99wd4wi8AfaSkqprWo206g" yt aesthetic)
-                     ;; ("https://theconversation.com/africa/home-page/news.atom" politics)
-                     ;; ("https://old.reddit.com/r/pureretention/.rss" social)
-
-                     ;; ("https://stackexchange.com/feeds/questions" cs)
                      ("https://www.youtube.com/feeds/videos.xml?channel_id=UCoxcjq-8xIDTYp3uz647V5A" yt math)))
-                     ;; ("https://www.nature.com/palcomms.rss" science)))
 
 
 (use-package elfeed-goodies
@@ -41,5 +32,17 @@
   (setq elfeed-search-filter "@1-month-ago +unread"))
 
 (add-hook 'elfeed-search-mode-hook #'elfeed-update)
+
+
+
+(defun salih/add-feeds-by-keyword (keyword category)
+  (let ((urls (list "https://hnrss.org/newest?q=%s")))
+    (dolist (url urls)
+      (let ((l (format url keyword)))
+       (add-to-list 'elfeed-feeds `(,l ,category))))))
+
+(salih/add-feeds-by-keyword "chomsky" 'philosophy)
+(salih/add-feeds-by-keyword "emacs" 'emacs)
+
 
 (provide '+feeds)
