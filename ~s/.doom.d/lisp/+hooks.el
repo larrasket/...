227823f393361249aafefa83259324fb60531488
Salih Muhammed <lr0@gmx.com>
2023-08-24 20:46:10 +0300

    chore(emacs): fix arabic font

--- ~s/.doom.d/lisp/+hooks.el
+++ ~s/.doom.d/lisp/+hooks.el
@@ -208,6 +208,7 @@
     (message "Compiled: %s" form-with-print)))
 
 (add-hook 'elfeed-show-mode-hook 'visual-line-mode)
+(add-hook 'elfeed-show-mode-hook (lambda () (set-fontset-font "fontset-default" 'arabic (font-spec :family "Arial" :size 16))))
 
 (add-hook 'eshell-alias-load-hook 'salih/eshell-load-bash-aliases)
 
