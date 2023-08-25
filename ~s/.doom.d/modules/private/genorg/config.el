12e9b5004d8c59cb8c800b280d48173a9baa4514
Salih Muhammed <lr0@gmx.com>
2023-08-25 04:09:46 +0300

    chore(emacs): disable underline font with org-link

--- ~s/.doom.d/modules/private/genorg/config.el
+++ ~s/.doom.d/modules/private/genorg/config.el
@@ -3,7 +3,8 @@
 (after! org
 
   (custom-set-faces
-   '(variable-pitch ((t (:family "Roboto Mono" :height 1.0 :weight normal))))
+   '(org-link ((t (:inherit link :underline nil :slant italic :weight bold))))
+   '(variable-pitch ((t (:family "Roboto Condensed" :height 1.0 :weight normal))))
    '(org-level-1 ((t (:inherit outline-1 :height 1.5 :weight normal :family "Arial"))))
    '(org-level-2 ((t (:inherit outline-2 :height 1.2 :weight normal :family "Arial"))))
    '(org-level-3 ((t (:inherit outline-3 :height 1.2 :weight normal :family "Arial"))))
