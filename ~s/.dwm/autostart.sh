e83b91434c491beddd93f476f5a4288a59754639
Salih Muhammed <lr0@gmx.com>
2023-09-27 19:57:58 +0300

    chore(dots): use blank bg in main

--- ~s/.dwm/autostart.sh
+++ ~s/.dwm/autostart.sh
@@ -10,8 +10,8 @@ xset r rate 200 25
 setxkbmap -layout us,ara -option 'grp:alt_shift_toggle'
 /home/l/.local/bin/pantalaimon &
 /home/l/go/bin/ang &
-# feh --bg-fill ~/configs/bg.png
-feh --bg-fill "$(find ~/me/links/art/land -type f | shuf -n 1)"
+feh --bg-fill ~/configs/bg.png
+# feh --bg-fill "$(find ~/me/links/art/land -type f | shuf -n 1)"
 # tor &
 # pgrep -fl 'pidswallow -gl' || pidswallow -gl
 #pantalaimon --data-path ~/tmp/pant/ &
