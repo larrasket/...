6b5ba3325a2822e6e4bdeebd064eae5e93b2766e
Salih Muhammed <lr0@gmx.com>
2023-10-07 13:03:10 +0300

    Update autostart script

--- ~s/.dwm/autostart.sh
+++ ~s/.dwm/autostart.sh
@@ -8,7 +8,6 @@ picom &
 dunst &
 xset r rate 200 25
 setxkbmap -layout us,ara -option 'grp:alt_shift_toggle'
-/home/l/.local/bin/pantalaimon &
 /home/l/go/bin/ang &
 feh --bg-fill ~/configs/bg.png
 # feh --bg-fill "$(find ~/me/links/art/land -type f | shuf -n 1)"
@@ -16,4 +15,5 @@ feh --bg-fill ~/configs/bg.png
 # pgrep -fl 'pidswallow -gl' || pidswallow -gl
 #pantalaimon --data-path ~/tmp/pant/ &
 flameshot &
+picom --xrender-sync-fence --vsync
 libinput-gestures-setup start
