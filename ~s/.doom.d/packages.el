c79e4b058a302384322f1bce4135c34fb587f9b2
Salih Muhammed <lr0@gmx.com>
2023-10-26 20:50:56 +0300

    Add packages comments

--- ~s/.doom.d/packages.el
+++ ~s/.doom.d/packages.el
@@ -1,77 +1,76 @@
-                                        ; -*- no-byte-compile: t; -*-
+(package! wakatime-mode)                ; voluntary tracking
+(package! org-web-tools)                ; viewing urls in org
 
-(package! wakatime-mode)
-
-(package! org-web-tools)
-
-(package! wiki-summary)
-
-(package! iedit)
-
-(package! git-auto-commit-mode)
-(package! highlight-indent-guides)
-(package! grip-mode)
-(package! column-enforce-mode)
+(package! wiki-summary)                 ; get a wiki line summary for new
+                                        ; topics. also ueful when writing notes
+                                        ; about new things
 
 
 
+(package! iedit)                        ; JetBrains' rename function. Also see `salih/rename-or-iedit`
+
+(package! git-auto-commit-mode)         ; auto commit
+
+(package! highlight-indent-guides)      ; this package needs a lot of
+                                        ; improvement or maybe it should be
+                                        ; replaced by something native by Emacs.
+                                        ; Yet it helps making it a little but
+                                        ; more modern
+                                        ;
+(package! grip-mode)                    ; I do not remember what's that for but
+                                        ; sounds useful
+(package! column-enforce-mode)          ; I use this instead of `display-fill-column-indicator`
+
+(package! go-translate)                 ; google API for lingustics
 
 
+(package! consult-org-roam)             ; useful org-roam functions
+
+(package! sage-shell-mode)              ; for me when I like to be a mathematician
 
 
-
-(package! go-translate)
-(package! distinguished-theme)
-
-(unpin! vertico)
-(package! vertico)
-
-(package! consult-org-roam)
-
-(package! sage-shell-mode)
-(package! leetcode)
 (package! awqat
   :recipe (:host github
-           :repo "luggages/awqat"))
-(package! ob-sagemath)
-(package! lsp-jedi)
+           :repo "luggages/awqat"))     ; obeying the almighty.
 
-
-(unpin! evil-collection)
+(unpin! evil-collection)                ; fixes evil for mu4e
 (package! evil-collection
   :recipe (:repo "luggages/evil-collection" :branch "mu4e-development"))
 
 
 (package! ox-tufte
   :recipe (:host github
-           :repo "luggages/ox-tufte"))
+           :repo "luggages/ox-tufte"))  ; for my website.
 
 (package! ox-html-stable-ids
   :recipe (:host github
            :repo "larrasket/ox-html-stable-ids.el"))
-(package! centaur-tabs)
 
-(package! vulpea)
+(package! centaur-tabs)                 ; nice tabs
 
-
-(package! nov)
+(package! vulpea)                       ; roam advanced functions
 
 
+(package! nov)                          ; browse epubs from emacs
 
-(package! org-bullets)
-(package! elfeed-tube)
+(package! org-bullets)                  ; nicer org view
 
-(package! maxima)
+(package! elfeed-tube)                  ; see more yt info from elfeed
+
+(package! maxima)                       ; a maxima repl.. not sure about it (and
+                                        ; about using maxima in general)
 
 
-(package! pretty-hydra)  ;; dependency
+(package! pretty-hydra)                 ; for the next line
 (package! org-media-note :recipe (:host github :repo "yuchen-lea/org-media-note"))
+
 (package! ob-julia-vterm)
-
-
 (unpin! ess)
 (package! ess)
 
-(package! kaolin-themes)
 
-(package! flycheck-projectile)
+
+(package! distinguished-theme)
+(package! kaolin-themes)
+(unpin! vertico)
+(package! vertico)
