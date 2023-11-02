db5677ecde9bb6f64065a6416b42a90287f70099
Salih Muhammed <lr0@gmx.com>
2023-11-02 18:16:33 +0200

    Modify IRC config

--- ~s/.doom.d/lisp/+erc.el
+++ ~s/.doom.d/lisp/+erc.el
@@ -1,18 +1,21 @@
 (require 'erc-services)
-;; (require '+erc-sasl)
+(require 'erc-sasl)
+
+
 
 (setq erc-nick                                          user-short-username
       erc-user-full-name                                user-full-name
       erc-prompt-for-password                           nil
       erc-prompt-for-nickserv-password                  nil
       erc-enable-logging                                t
-      erc-log-channels-directory                        "~/log"
+      erc-log-channels-directory                        "~/me/irc-log"
+      erc-quit-reason                                   'salih/quit-erc
       erc-hide-list                                     '("JOIN" "PART" "MODE"
-                                                          "324"   ; modes
-                                                          "329"   ; channel creation date
-                                                          "332"   ; topic notice
-                                                          "333"   ; who set the topic
-                                                          "353")  ; Names notice
+                                                          "324" ; modes
+                                                          "329" ; channel creation date
+                                                          "332" ; topic notice
+                                                          "333" ; who set the topic
+                                                          "353") ; Names notice
 
       erc-autojoin-channels-alist                       '((Libera.Chat
                                                            "##tea"
@@ -22,36 +25,22 @@
                                                            "#emacs"
                                                            "##arabic")))
 
-;; (add-to-list 'erc-sasl-server-regexp-list "irc\\.libera\\.chat")
 
+(defun salih/erc-connect ()
+    "Connect to Ement with credentials from Authinfo."
+  (interactive)
+  (let* ((auth-info (auth-source-search :host "irc.libera.chat"
+                                        :require '(:user :secret)))
+         (user-id (plist-get (car auth-info) :user))
+         (password (funcall (plist-get (car auth-info) :secret))))
+    (erc-tls       :user     user-id
+                   :password password)))
 
+(defun salih/quit-erc (s)
+  "quit quote"
+  "I seek refuge in God, from Satan the rejected")
 
-;; (defun erc-login ()
-;;   "Perform user authentication at the IRC server. (PATCHED)"
-;;   (erc-log (format "login: nick: %s, user: %s %s %s :%s"
-;;                    (erc-current-nick)
-;;                    (user-login-name)
-;;                    (or erc-system-name (system-name))
-;;                    erc-session-server
-;;                    erc-session-user-full-name))
-;;   (if erc-session-password
-;;       (erc-server-send (concat "PASS :" (erc--unfun erc-session-password)))
-;;     (message "Logging in without password"))
-;;   (when (and (featurep '+erc-sasl)
-;;              (erc-sasl-use-sasl-p))
-;;     (erc-server-send "CAP REQ :sasl"))
-;;   (erc-server-send (format "NICK %s" (erc-current-nick)))
-;;   (erc-server-send
-;;    (format "USER %s %s %s :%s"
-;;            ;; hacked - S.B.
-;;            erc-session-username
-;;            "0" "*"
-;;            erc-session-user-full-name))
-;;   (erc-update-mode-line))
-
-
-
-
+ 
 (after! erc
   (erc-spelling-mode)
   (erc-log-mode))
