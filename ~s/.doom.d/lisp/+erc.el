36b2a4f51f89630ca7c0d818f82e3a3b248b8432
Salih Muhammed <lr0@gmx.com>
2023-11-02 16:33:14 +0200

    update erc

--- ~s/.doom.d/lisp/+erc.el
+++ ~s/.doom.d/lisp/+erc.el
@@ -1,4 +1,5 @@
 (require 'erc-services)
+;; (require '+erc-sasl)
 
 (setq erc-nick                                          user-short-username
       erc-user-full-name                                user-full-name
@@ -6,19 +7,54 @@
       erc-prompt-for-nickserv-password                  nil
       erc-enable-logging                                t
       erc-log-channels-directory                        "~/log"
-      erc-hide-list                                     '("JOIN" "PART"
-                                                          "QUIT" "MODE")
+      erc-hide-list                                     '("JOIN" "PART" "MODE"
+                                                          "324"   ; modes
+                                                          "329"   ; channel creation date
+                                                          "332"   ; topic notice
+                                                          "333"   ; who set the topic
+                                                          "353")  ; Names notice
+
       erc-autojoin-channels-alist                       '((Libera.Chat
                                                            "##tea"
-                                                           "#erc"
                                                            "#org-mode"
                                                            "#go-nuts"
                                                            "#archlinux-offtopic"
-                                                           "##chat"
                                                            "#emacs"
                                                            "##arabic")))
 
-(after! erc (erc-spelling-mode)
+;; (add-to-list 'erc-sasl-server-regexp-list "irc\\.libera\\.chat")
+
+
+
+;; (defun erc-login ()
+;;   "Perform user authentication at the IRC server. (PATCHED)"
+;;   (erc-log (format "login: nick: %s, user: %s %s %s :%s"
+;;                    (erc-current-nick)
+;;                    (user-login-name)
+;;                    (or erc-system-name (system-name))
+;;                    erc-session-server
+;;                    erc-session-user-full-name))
+;;   (if erc-session-password
+;;       (erc-server-send (concat "PASS :" (erc--unfun erc-session-password)))
+;;     (message "Logging in without password"))
+;;   (when (and (featurep '+erc-sasl)
+;;              (erc-sasl-use-sasl-p))
+;;     (erc-server-send "CAP REQ :sasl"))
+;;   (erc-server-send (format "NICK %s" (erc-current-nick)))
+;;   (erc-server-send
+;;    (format "USER %s %s %s :%s"
+;;            ;; hacked - S.B.
+;;            erc-session-username
+;;            "0" "*"
+;;            erc-session-user-full-name))
+;;   (erc-update-mode-line))
+
+
+
+
+(after! erc
+  (erc-spelling-mode)
   (erc-log-mode))
+
 
 (provide '+erc)
