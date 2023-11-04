0c13b437e60e666b717b3c7e490806f07578cec0
Salih Muhammed <lr0@gmx.com>
2023-11-04 19:51:30 +0200

    Load customs

---
+++ ~s/.doom.d/custom.el
@@ -0,0 +1,38 @@
+(put 'customize-group 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(all-the-icons-completion-mode nil)
+ '(custom-safe-themes
+   '("58264887d7ab17702ef85bbd96e11bd7f613622ff9c63990be860b958c978f09" "21e3d55141186651571241c2ba3c665979d1e886f53b2e52411e9e96659132d4" "3d94d6d1a1c23113a60c8496c9aed094dbc2695f219e8127bb168d17b1e6dab3" "9b64a681308383067359cf06bfa6a1bc4fa75c5b68182e4d6ba4d1816277d70e" "3454885b915a176dce4b53e35053b7ee0aa9362fb9e934057ac44b6842a97453" "46325e20421d2b4423cc90db5a35e0a45bd78a6f3e26c52314b189af3cc00733" "30b323c73d2b24c3eff96802f0085fbfef3c6d2aad03ddfb5f615b658b2d5bfa" "e8ab68ce371f48623dab9658d7910458e98affec3e09585a39552dbd3fd1ecda" "64204b9e3ad01000654d5524d2904fc8fa28aafc168f48660897ddfe36a2bfd5" "8e08bb8da358e2cf92e10e4bac47b025ccbcf4c70788cdbd67dc4ed11f786194" "82f0f47ac811eeb45fbcfc5fee48989e4d0bca11f74653b838c29fab9a20aee7" "d31c3706f7c1b0520405c796b33f515bc481d2062cbc964f3c36925665999a6d" "454e92bc5f22f690afce91cb6f92a3ccb638c57a89e84c55759fb16dfb2444e4" "6c01b5d4faa0f143055e63c9fba8e23e9160f181e54b14b46d56410811edbc9e" "6ed8a3705a4296955010ecfcf808f02ac0d52985373e07c63f7fe5bc85206bb4" "ed6e47baf355da248c4de8953058234b82c8f838fc85f570f9fe1700e47b9426" "7021d655cda2ed5476960e166350d6cf9ee8af874216a26bc5457591222bb330" "f7b6b207d7a6318ea5d33ca2dea51483350d0c26beb986f008d63258b9c112ab" "d9c038dc91688c433de8e83709449563ec6475b900a21d7016856035ae4dcd32" "44b463a6e1111340cee98070c9492d95d8c2fcd7ad9397541687a06c24ad85cf" "0d12b08dec64641c5df1a13d2c52ad678f6235a9b1c86041ea457fc1a71651dc" "2d09bd884d697b48b380b48117ccaebd8e99fe1cb242e31675dcec5724c603f6" "f84dbe5cfa80aa6774c57fef30d76bcdeb71bd0077665fb74f75728c42f5675d" "2ef84b2c7ad4810912a095993ca8bdf386e1fd7f97842b57aac62dddb2bba211" "71acf47cc8cd4158e52ef63a9f8c4d128aa33d6772a0106b5a72757486047e08" "65a1a112abd99456167df57ce2cfff42ed137c4f9146e75b2ae9812499689c3a" "611ef0918b8b413badb8055089b5499c1d4ac20f1861efba8f3bfcb36ad0a448" "15604b083d03519b0c2ed7b32da6d7b2dc2f6630bef62608def60cdcf9216184" "88cb0f9c0c11dbb4c26a628d35eb9239d1cf580cfd28e332e654e7f58b4e721b" "69f7e8101867cfac410e88140f8c51b4433b93680901bb0b52014144366a08c8" "4b026ac68a1aa4d1a91879b64f54c2490b4ecad8b64de5b1865bca0addd053d9" "062e6ec918ed89d5d9a342dbbefd99e8690c5514c6698a78fc25f259972e9242" "f23398a9a8d9476cd5ffdd04f55159bd0c454a9d9ba3de145d637c42c3becddd" "1cae4424345f7fe5225724301ef1a793e610ae5a4e23c023076dc334a9eb940a" default))
+ '(indent-bars-highlight-current-depth nil)
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(modus-themes-bold-constructs t)
+ '(modus-themes-fringes nil)
+ '(modus-themes-italic-constructs t)
+ '(modus-themes-mode-line '(moody borderless))
+ '(modus-themes-org-blocks 'gray-background)
+ '(safe-local-variable-values '((org-download-image-dir . "../i") (salih/rebuild . t))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-buffer-modified ((t (:inherit (doom-modeline-urgent)))))
+ '(git-gutter-fr:added ((((class color) (min-colors 89)) :foreground "#006700")))
+ '(git-gutter-fr:modified ((((class color) (min-colors 89)) :foreground "#6f4f00")))
+ '(org-document-title ((t (:inherit outline-8 :height 1.8 :weight bold))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5 :weight bold))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.2 :weight bold))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2 :weight bold))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0 :weight bold))))
+ '(org-level-5 ((t (:inherit outline-5 :height 0.9 :weight bold))))
+ '(org-level-6 ((t (:inherit outline-6 :height 0.8 :weight bold))))
+ '(org-level-7 ((t (:inherit outline-7 :height 0.7 :weight bold))))
+ '(org-level-8 ((t (:inherit outline-8 :height 0.6 :weight bold))))
+ '(org-link ((t (:inherit link :underline nil :foreground "#79b58f" :slant normal :weight bold :family "Pragmata Pro"))))
+ '(org-roam-dailies-calendar-note ((t (:inherit link :underline nil))))
+ '(variable-pitch ((t (:family "DejaVu Sans")))))
+(put 'customize-themes 'disabled nil)
