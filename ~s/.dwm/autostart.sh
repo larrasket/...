e0f5a4e2e187c1e899d955986e5f3a804c1fbed9
Salih Muhammed <lr0@gmx.com>
2023-11-07 22:11:56 +0200

    Update startup

--- ~s/.dwm/autostart.sh
+++ ~s/.dwm/autostart.sh
@@ -3,18 +3,8 @@ for f in /etc/X11/xinit/xinitrc.d/*.sh; do
     source "$f"
 done
 ~/.dwm/screen.sh
-picom &
-#--experimental-backends &
+~/.dwm/keyboard.sh
 dunst &
-xset r rate 200 25
-setxkbmap -layout us,ara -option 'grp:alt_shift_toggle'
-# /home/l/go/bin/ang &
 feh --bg-fill ~/configs/~s/bg.png
-#feh --bg-fill "$(find ~/me/links/art/land -type f | shuf -n 1)"
-# tor &
-# pgrep -fl 'pidswallow -gl' || pidswallow -gl
-#pantalaimon --data-path ~/tmp/pant/ &
 flameshot &
-picom --xrender-sync-fence --vsync
-xmodmap ~/.Xmodmap
-
+picom --xrender-sync-fence --vsync &
