ce853303f842ea67874c10b84f306d707c14ea37
Salih Muhammed <lr0@gmx.com>
2023-11-07 03:07:31 +0200

    Update irc config

--- ~s/.doom.d/lisp/+erc.el
+++ ~s/.doom.d/lisp/+erc.el
@@ -1,6 +1,4 @@
 (require 'erc-services)
-(require 'erc-sasl)
-(require '+erc-sasl)
 
 
 (setq erc-nick                                          user-short-username
@@ -10,7 +8,10 @@
       erc-enable-logging                                t
       erc-log-channels-directory                        "~/me/irc-log"
       erc-quit-reason                                   'salih/quit-erc
-      erc-hide-list                                     '("JOIN" "PART" "MODE" "QUIT"
+      erc-hide-list                                     '("JOIN"
+                                                          "PART"
+                                                          "MODE"
+                                                          "QUIT"
                                                           "324"  ; modes
                                                           "329"  ; channel creation date
                                                           "332"  ; topic notice
@@ -39,36 +40,42 @@
   "I seek refuge in God, from Satan the rejected")
 
 
+
 (after! erc
-  (erc-spelling-mode)
-  (erc-log-mode))
+ (require 'erc-sasl)
+ (require '+erc-sasl)
+
+ (add-to-list 'erc-sasl-server-regexp-list "irc\\.freenode\\.net")
+ (add-to-list 'erc-sasl-server-regexp-list "irc\\.libra\\.chat")
+ (add-to-list 'erc-sasl-server-regexp-list "irc\\.oftc\\.net")
 
 
-(add-to-list 'erc-sasl-server-regexp-list "irc\\.freenode\\.net")
-(add-to-list 'erc-sasl-server-regexp-list "irc\\.libra\\.chat")
-(add-to-list 'erc-sasl-server-regexp-list "irc\\.oftc\\.net")
+ (defun erc-login ()
+   "Perform user authentication at the IRC server."
+   (erc-log (format "login: nick: %s, user: %s %s %s :%s"
+                    (erc-current-nick)
+                    (user-login-name)
+                    (or erc-system-name (system-name))
+                    erc-session-server
+                    erc-session-user-full-name))
+   (if erc-session-password
+       (erc-server-send (concat "PASS :" (erc--unfun erc-session-password)))
+     (message "Logging in without password"))
+   (when (and (featurep 'erc-sasl))
+     (erc-server-send "CAP REQ :sasl"))
+   (erc-server-send (format "NICK %s" (erc-current-nick)))
+   (erc-server-send
+    (format "USER %s %s %s :%s"
+            erc-session-username
+            "0" "*"
+            erc-session-user-full-name))
+   (erc-update-mode-line))
+ (erc-spelling-mode)
+ (erc-log-mode))
 
 
-(defun erc-login ()
-  "Perform user authentication at the IRC server."
-  (erc-log (format "login: nick: %s, user: %s %s %s :%s"
-                   (erc-current-nick)
-                   (user-login-name)
-                   (or erc-system-name (system-name))
-                   erc-session-server
-                   erc-session-user-full-name))
-  (if erc-session-password
-      (erc-server-send (concat "PASS :" (erc--unfun erc-session-password)))
-    (message "Logging in without password"))
-  (when (and (featurep 'erc-sasl) (erc-sasl-use-sasl-p))
-    (erc-server-send "CAP REQ :sasl"))
-  (erc-server-send (format "NICK %s" (erc-current-nick)))
-  (erc-server-send
-   (format "USER %s %s %s :%s"
-           erc-session-username
-           "0" "*"
-           erc-session-user-full-name))
-  (erc-update-mode-line))
+
+
 
 
 
