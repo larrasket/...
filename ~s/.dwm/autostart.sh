b93768bc443a2f6e8422c8b3deea44af73ae5686
Salih Muhammed <lr0@gmx.com>
2024-01-04 14:32:09 +0200

    Auto start rygel

--- ~s/.dwm/autostart.sh
+++ ~s/.dwm/autostart.sh
@@ -5,6 +5,7 @@ done
 ~/.dwm/screen.sh
 ~/.dwm/keyboard.sh
 dunst &
-feh --bg-fill ~/configs/~s/bg.png
+feh --bg-fill ~/configs/~s/bg.png &
 flameshot &
 picom --xrender-sync-fence --vsync &
+rygel -g 5 &
