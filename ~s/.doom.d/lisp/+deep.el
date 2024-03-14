94f326519c8661596aede5505722ea6f4492d809
Salih Muhammed <lr0@gmx.com>
2024-03-14 13:27:17 +0200

    Remove duplicate code

--- ~s/.doom.d/lisp/+deep.el
+++ ~s/.doom.d/lisp/+deep.el
@@ -9,15 +9,7 @@
 ;; (assoc-delete-all "Jump to bookmark"            +doom-dashboard-menu-sections)
 (custom-set-variables '(all-the-icons-completion-mode nil))
 
-(if (or (eq doom-theme 'modus-vivendi-tritanopia)
-        (eq doom-theme 'modus-vivendi-deuteranopia)
-        (eq doom-theme 'modus-vivendi))
-    (progn
-      (set-frame-parameter nil 'alpha-background 95)
-      (add-to-list 'default-frame-alist '(alpha-background . 95)))
-  (progn
-      (set-frame-parameter nil 'alpha-background 100)
-      (add-to-list 'default-frame-alist '(alpha-background . 100))))
+
 
   
 
