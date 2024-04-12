4e456e2a7d3c46516a8e2b71e15700738bbfb689
Salih Muhammed <lr0@gmx.com>
2024-04-12 22:53:56 +0200

    Fix notes to stats

--- ~s/.doom.d/lisp/+advice.el
+++ ~s/.doom.d/lisp/+advice.el
@@ -4,10 +4,16 @@
 (advice-add 'org-agenda                         :before #'vulpea-agenda-files-update)
 (advice-add 'org-clock-in                       :before #'salih/toggle-logbook-on)
 (advice-add 'org-clock-in                       :after  #'salih/toggle-stats-on)
+(advice-add 'org-add-note                       :before #'salih/toggle-stats-on)
+(advice-add 'org-add-note                       :after  #'salih/toggle-logbook-on)
 (advice-add 'org-todo-list                      :before #'vulpea-agenda-files-update)
 (advice-add 'org-agenda-quit                    :before #'org-save-all-org-buffers)
+(advice-add 'org-log-beginning                  :before #'salih/toggle-stats-on)
+(advice-add 'org-log-beginning                  :after  #'salih/toggle-logbook-on)
 (advice-add 'org-agenda-add-note                :before #'salih/toggle-stats-on)
 (advice-add 'org-agenda-add-note                :after  #'salih/toggle-logbook-on)
+
+
 (advice-add 'org-media-note-insert-link         :around #'salih/org-media-note-insert-link)
 (advice-add 'org-ql-view--format-element        :around #'salih/org-ql-view--format-element)
 (advice-add 'sly-compile-string                 :before #'salih/sly--compile-eval-begin-print)
