16c9797e1e0515c6e926523b9058c82ac9ff0074
Salih Muhammed <lr0@gmx.com>
2024-09-30 01:21:25 +0300

    Update ERC

--- ~s/.doom.d/lisp/+erc.el
+++ ~s/.doom.d/lisp/+erc.el
@@ -1,4 +1,5 @@
 (require 'erc-services)
+(require 'erc-sasl)
 
 (setq erc-nick                                          user-short-username
       erc-user-full-name                                user-full-name
@@ -7,6 +8,7 @@
       erc-enable-logging                                t
       erc-log-channels-directory                        "~/me/irc-log"
       erc-quit-reason                                   'salih/quit-erc
+      erc-sasl-mechanism                                'scram
       erc-hide-list                                     '("JOIN"
                                                           "PART"
                                                           "MODE"
@@ -23,9 +25,26 @@
                                                            "#emacs"
                                                            "##arabic")))
 
-
 (defun salih/quit-erc (s)
   "quit cute quote"
   "I seek refuge in God, from Satan the rejected")
+
+(defun salih/lookup-password (host user port)
+  (let ((auth (auth-source-search :host host :user user :port port)))
+    (if auth
+        (let ((secretf (plist-get (car auth) :secret)))
+          (if secretf
+              (funcall secretf)
+            (error "Auth entry for %s@%s:%s has no secret!"
+                   user host port)))
+      (error "No auth entry found for %s@%s:%s" user host port))))
+
+(defun irc (&optional arg)
+    (interactive "P")
+    (let ((pass (salih/lookup-password "irc.libera.chat" "lr0" 80)))
+      (erc
+       :server "irc.libera.chat"
+       :nick user-short-username
+       :password pass)))
 
 (provide '+erc)
