fc0fc01be990fb844101acb6b1455afa5973a9d4
Salih Muhammed <lr0@gmx.com>
2024-12-08 15:15:33 +0200

    Update music library

--- ~s/.config/cmus/lib.pl
+++ ~s/.config/cmus/lib.pl
@@ -2,3 +2,4 @@
 /home/l/me/music/fz/fz/فيروز   يا حجل صنين.mp3
 /home/l/me/music/03/فدوى المالكي⧸ وحقك أنت المنى والطلب.mp3
 /home/l/me/music/12nov/Vivaldi Concerto ''Alla rustica'' for strings & continuo in G major RV 151.mp3
+/home/l/me/music/mutanabi/المتنبي ( تجري الرياح بما لا تشتهي السفن ).mp3
