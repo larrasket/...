a4b3fd71a307a2ef60309f8ab5a34f3df5a21d19
Salih Muhammed <lr0@gmx.com>
2025-01-14 16:15:22 +0200

    Remove dwm

--- ~s/.dwm/keyboard.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-xset r rate 200 25
-setxkbmap -layout us,ara -option 'grp:alt_shift_toggle'
--- ~s/dwm/.dir-locals.el
+++
@@ -1,6 +0,0 @@
-;;; Directory Local Variables            -*- no-byte-compile: t -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((c-mode . ((eval . (format-all-mode -1))
-            (eval . (lsp-mode -1))
-            (eval . (flycheck-mode -1)))))
