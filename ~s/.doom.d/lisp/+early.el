c3c1d8c79a3aafeb51189e85f65188d41a02b541
Salih Muhammed <lr0@gmx.com>
2025-07-04 20:54:00 +0300

    Tidy

--- ~s/.doom.d/lisp/+early.el
+++ ~s/.doom.d/lisp/+early.el
@@ -8,10 +8,11 @@ that used by the user's shell.
 This is particularly useful under Mac OS X and macOS, where GUI
 apps are not started from a shell."
   (interactive)
-  (let ((path-from-shell (replace-regexp-in-string
-			  "[ \t\n]*$" "" (shell-command-to-string
-					  "$SHELL --login -c 'echo $PATH'"
-						    ))))
+  (let ((path-from-shell
+         (replace-regexp-in-string
+          "[ \t\n]*$" ""
+          (shell-command-to-string
+           "$SHELL --login -c 'echo $PATH'"))))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
 
