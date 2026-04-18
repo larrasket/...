a30295bad441b81ffc6c4eb8b6448974d184d9d4
Salih Muhammed <root@lr0.org>
2026-04-18 18:38:03 +0200

    Add aerosapce

---
+++ ~s/.config/aerospace/focus-mail.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source "$(dirname "$0")/lib.sh"
+focus_or_open "W" "open -a Mail" 'com\.apple\.mail'
---
+++ ~s/.config/aerospace/focus-music.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source "$(dirname "$0")/lib.sh"
+focus_or_open "M" "open -a Music" 'com\.apple\.Music|com\.nadroju\.silicio'
